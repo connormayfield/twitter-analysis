f5070734f40cf3824766f1ed026edec3ac989eb5
shirano2 <shirano2@naver.com>
2019-05-09 20:21:12 -0500

    side bar design changed

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -5,10 +5,10 @@ export function Navbar({props}){
 
     return (<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
     
-    <Link className="navbar-brand" to="/">TweetTrace</Link>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <Link className="navbar-brand" to="/"><span style={{display:"inline-block", width:"60px"}}></span><span style={{"font-size":"25px", "color":"white"}}>TweetTrace</span></Link>
+    {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
-    </button>
+    </button> */}
     
   </nav>
   )
