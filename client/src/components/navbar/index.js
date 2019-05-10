35929b5f6664b8e51c47eff3adc2be4e421f3781
David Toth <dtoth2016@gmail.com>
2019-05-10 10:17:50 -0500

    commit before switching

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -7,15 +7,15 @@ export function Navbar({props}){
 
     return (
     
-    <nav className="navbar navbar-dark bg-primary">
-      <Container>
-        <button className="navbar-toggler" id="toggle-sidebar" type="button">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <Link className="navbar-brand" to="/">TweetTrace</Link>
-      </Container>
-    </nav>
-  )
+      <nav className="navbar navbar-dark bg-primary">
+        <Container>
+          <button className="navbar-toggler" id="toggle-sidebar" type="button">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <Link className="navbar-brand" to="/">TweetTrace</Link>
+        </Container>
+      </nav>
+    )
 }
 
 export default Navbar
